ca6da9499a89d012bce7424efd30e5a0662f1277
Nguyenvanhieu2403 <Nguyenvanhieu2422003@gmail.com>
2024-01-27 20:01:24 +0700

    (client) End of section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response =>  {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
